2047d89997479f6e13ab81e7017e920172fc5ebe
Sonali Misra <sonalimisra2001@gmail.com>
2024-02-18 20:02:37 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-def data_render(request):
-    data='We are assuming that we got the data from database'
-    d={'assumption':data}
-    return render(request,'data_render.html',context=d)
+def conditional(request):
+    d={'a':100,'b':50,'c':500}
+    return render(request,'conditional.html',d)
